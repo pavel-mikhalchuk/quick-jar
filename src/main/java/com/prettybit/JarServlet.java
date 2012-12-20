0389cc3621bc2dd8c34dd9b94fcbcacaf08a138f
pavel-mikhalchuk <pavel.mikhalchuk@gmail.com>
2012-12-20 00:08:20 -0800

    [Jar Servlet]: jar file generation

--- src/main/java/com/prettybit/JarServlet.java
+++ src/main/java/com/prettybit/JarServlet.java
@@ -1,13 +1,17 @@
 package com.prettybit;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * @author Pavel Mikhalchuk
@@ -16,37 +20,56 @@ public class JarServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String className = request.getParameter("class");
         String code = request.getParameter("code");
-        File clazz = store(code);
+
+        File clazz = store(className, code);
         File byteCode = compile(clazz);
         File jar = makeJar(byteCode);
 
-        write(clazz, response);
+        write(jar, response);
 
-        clazz.delete();
-        //byteCode.delete();
-        //jar.delete();
+        FileUtils.forceDelete(clazz);
+        FileUtils.forceDelete(byteCode);
+        FileUtils.forceDelete(jar);
     }
 
-    private File store(String code) throws IOException {
-        File file = File.createTempFile("code", ".java");
+    private File store(String className, String code) throws IOException {
+        File file = new File(FileUtils.getTempDirectory(), className + ".java");
         FileUtils.write(file, code);
         return file;
     }
 
-    private File compile(File clazz) {
-        return null;
+    private File compile(File clazz) throws IOException {
+        ProcessBuilder builder = new ProcessBuilder("/usr/bin/javac", "-verbose", clazz.getAbsolutePath());
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+        printProcessOutput(process.getInputStream());
+        return new File(FilenameUtils.removeExtension(clazz.getAbsolutePath()) + ".class");
     }
 
-    private File makeJar(File byteCode) {
-        return null;
+    private File makeJar(File byteCode) throws IOException {
+        File jar = new File(FileUtils.getTempDirectory(), "JAR.jar");
+        ProcessBuilder builder = new ProcessBuilder("/usr/bin/jar", "cvf", jar.getAbsolutePath(), byteCode.getAbsolutePath());
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+        printProcessOutput(process.getInputStream());
+        return jar;
     }
 
     private void write(File jar, HttpServletResponse response) throws IOException {
         response.setContentLength((int) jar.length());
         response.setContentType("application/java-archive");
-        response.setHeader("Content-Disposition", "attachment; filename=\"jar.jar\"");
+        response.setHeader("Content-Disposition", "attachment; filename=\"JAR.jar\"");
         response.getOutputStream().write(FileUtils.readFileToByteArray(jar));
+    }
+
+    private void printProcessOutput(InputStream stream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
     }
 
 }
---
+++ src/main/java/com/prettybit/Test.java
@@ -0,0 +1,12 @@
+package com.prettybit;
+
+/**
+ * @author Pavel Mikhalchuk
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("First Quick Jar");
+    }
+
+}
