60aee380f12b54398d51a7e8b9f6c8e0f890ff38
pavel-mikhalchuk <pavel.mikhalchuk@gmail.com>
2012-12-24 18:27:19 -0800

    + BoA test

---
+++ src/main/java/com/prettybit/quickjar/test/Test.java
@@ -0,0 +1,11 @@
+package com.prettybit.quickjar.test;
+
+/**
+ * @author Pavel Mikhalchuk
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("First Quick Jar");
+    }
+}
